af685dd5f37c84c65fd4ca96b5dc9d20fb050cb1
silan4 <urensilan416@gmail.com>
2024-05-08 17:38:07 +0300

    first commit

---
+++ scripts/constants.js
@@ -0,0 +1,29 @@
+//ekrana basılacak butonların verisi
+
+export const buttonData = [
+
+    {
+        id: 1 ,
+        text : 'Hepsi',
+        value: 'all',
+    },
+
+    {
+        id: 2,
+        text: 'Kahvaltı',
+        value: 'breakfast',
+    },
+
+    {
+        id:3,
+        text: 'Öğlen Yemeği',
+        value: 'lunch',
+    },
+    
+    {
+        id:4,
+        text: 'İçecekler',
+        value: 'shakes',
+    },
+
+];
